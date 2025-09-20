649b243069c510fce5ad13d2ca6ffd20af5169f5
ZEINZH <91441817+suryanicholas@users.noreply.github.com>
2025-09-20 21:11:10 +0700

    sd

--- client.cjs
+++ client.cjs
@@ -8,7 +8,7 @@ const http = require('http');
 
 const server = http.createServer();
 
-server.listen(3000, '0.0.0.0', () => {
+server.listen(3000, '127.0.0.1', () => {
     console.log('Server Running...');
 })
 
